e03ea69e48cd3bc6d7071be141fd433738bb127d
Gergely Nagy <nagyger94@gmail.com>
2023-04-10 11:09:00 +0200

    Létrehoztam a profil alapját, automatikusan generálódnak az adatok - ha kézzel be van írva a felhasználónév. Az alap profilkép attól függően változik, hogy férfi vagy női profil jön-e szembe. Alap formázások css segítségével.

---
+++ php/profile.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Gombos Géza és Nagy Gergely">
+    <meta name="description" content="Kate Smith fotográfus oldala">
+    <meta name="keywords" content="fotográfia,fotók,képek,művészet,rendezvény fotózás">
+    <title>Kate Smith Photography | Moments Worth Remembering</title>
+    
+    <link rel="icon" href="../img/camera_icon.png">
+    <link rel="stylesheet" href="../css/style.css">
+    <!--Google betűtípus himportálása-->
+    <link href="https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,200;0,300;0,400;0,500;1,300;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet"> 
+    
+    <!--Responsive dizájnhoz-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a id="aktiv_oldal" href="kezdolap.php">Kezdőlap</a></li>
+            <li><a href="rolam.php">Rólam</a></li>
+            <li><a href="referenciak.php">Referenciák</a></li>
+            <li><a href="szolgaltatasok.php">Szolgáltatások</a></li>
+            <li><a href="arajanlat.php">Árajánlatkérés</a></li>
+        </ul>
+    </nav>
+    
+    <header>
+        <div id="borito_kontener">
+            <!--
+                <h1 id="focim">Katie Smith Photograpy</h1>
+            -->
+            <img src="../img/katie_smith_logo.png" alt="Katie logo">
+        </div>    
+    </header>
+
+    <main>
+
+        <?php
+            require "form-methods.php";
+
+            $users = loadUsers();
+
+            foreach($users as $user){
+                if($user["username"] === "ozuu444"){ //ide kell majd a bejelentkezett felhasználónevet dinamikusan berakni
+                    $name = $user["name"];
+                    $username= $user["username"];
+                    $email = $user["email"];
+                    if($user["sex"] === "m"){
+                        $gender = "férfi";
+                    } elseif($user["sex"] === "f") {
+                        $gender = "nő";
+                    }
+                    
+                }
+            }
+
+            echo '<div id="profile_container">
+                    <h2>'.$name.' profilja</h2>
+                    <img id="avatar" src="../img/profile-pictures/'.$gender.'-avatar.png" width=10% alt="Férfi avatar">
+                    
+                    <div id="personal-info-container">
+                        <p>Név: ' .$name. '</p>
+                        <p>Felhasználónév: ' .$username. '</p>
+                        <p>Email: ' .$email. '</p>
+                        <p>Neme: '.$gender.'</p>
+                    </div>
+                </div>';
+                
+        ?>
+            
+
+
+
+    </main>
+
+    <footer>
+        <div class="footer_oszlop">
+            <h2>Kövess minket:</h2>
+            <ol>
+                <li><a href="https://www.facebook.com/"><img src="../img/kapcsolat/facebook.png" alt=""></a></li>
+                <li><a href="https://www.instagram.com/"><img src="../img/kapcsolat/instagram_logo.png" alt=""></a></li>
+                <li><a href="https://www.twitter.com/"><img src="../img/kapcsolat/twitter.png" alt=""></a></li>
+            </ol>
+        </div>
+
+        <div id="home">
+            <a href="#borito_kontener"><img src="../img/kapcsolat/nyil.png" alt="Az oldal tetejére" title="Az oldal tetejére"></a>
+        </div>
+
+        <div id="hirlevel" class="footer_oszlop">
+            <h2>Iratkozz fel hírlevelünkre:</h2>
+            <form action="feldolgoz.php" method="POST">
+                <label for="footer_input">Email:</label>
+                <input id="footer_input" type="email" name="email_add" size="30" maxlength="30">
+                <label> <input id="footer_submit" type="submit" value="Küldés"></label>
+            </form>
+        </div>
+        
+        <div id="copyright">
+           <p> minden jog fenntartva © 2023</p>
+           
+        </div>
+    </footer>
+</body>
+</html>
